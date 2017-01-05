c29835cd6e2f9de2216769d227d8fdfd4c29e106
Fabricio Teixeira Nascimento <fabriciotn@yahoo.com.br>
2017-01-05 07:12:20 -0200

    Criação da tela de login e dashboard

--- app/src/main/java/br/com/ttfinformatica/tripcontrol/Login.java
+++ app/src/main/java/br/com/ttfinformatica/tripcontrol/Login.java
@@ -1,13 +1,37 @@
 package br.com.ttfinformatica.tripcontrol;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
+
+    private EditText usuario;
+    private EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
+
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
+
+    public void entrarOnClick(View v){
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
+
+        if(usuarioInformado.equals("leitor") &&
+                senhaInformada.equals("123")){
+            startActivity(new Intent(this, DashboardActivity.class));
+        }else{
+            String mensagemErro = getString(R.string.erro_autenticacao);
+            Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
